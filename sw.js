3836758f80b8d3cdd742e0543b0bc67b765211dd
oLdpZ <old_p@oLdpZ_gaming.(none)>
2026-04-08 18:21:25 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => e.waitUntil(caches.open('atdw').then(c => c.addAll(['/']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
