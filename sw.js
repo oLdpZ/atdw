c8e2d264e5555fe3b8466afc79bd2f28308451e5
oLdpZ <old_p@oLdpZ_gaming.(none)>
2026-04-09 01:15:09 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,2 +1,23 @@
-self.addEventListener('install', e => e.waitUntil(caches.open('atdw').then(c => c.addAll(['/']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'atdw-v1.0.6b';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
